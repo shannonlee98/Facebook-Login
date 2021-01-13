7047dfb50e9dae92a631ce433695392d3d2cd6d6
shannonlee98 <shanklee.lj@gmail.com>
2021-01-13 09:34:07 +0800

    Cleanup routes

--- postHandler.js
+++ postHandler.js
@@ -1,7 +1,30 @@
-module.exports = function post(con) {
-  console.log('Connected to post')
-  var query = "INSERT INTO user (email, password) VALUES ('test@email.com', 'asdf1234')"
+
+const mysql = require('mysql')
+const db = require('./module')
+const con = require('./database')
+
+function getHandler(req, res) {
+  var query = `SELECT * FROM user WHERE email = '${req.body.email}'`
+  con.query(query, (err, result, fields) => {
+    var user = result[0]
+    res.send(user)
+  })
+}
+
+function postHandler(req, res) {
+  var email = req.body.email
+  var password = req.body.password
+  var query = `INSERT INTO user (email, password) VALUES ('${email}', '${password}')`
   con.query(query, function (req, res) {
     console.log('Insert row')
   })
+  res.send('Post api: ' + email + ' ' + password)
 }
+
+
+module.exports = {
+  postHandler,
+  getHandler
+}
+
+
--- routes.js
+++ routes.js
@@ -1,41 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const postHandler = require('./postHandler')
-
-const mysql = require('mysql')
-const db = require('./module')
-const con = mysql.createConnection({
-  host: db.host,
-  user: db.user,
-  password: db.password,
-  database: db.database
-})
-con.connect()
+const postHandler = require('./postHandler').postHandler
+const getHandler = require('./postHandler').getHandler
 
 router.use(express.urlencoded({ extended: true }))
 router.use(express.json())
 
-// define the home page route
-router.get('/', function (req, res) {
-  var query = `SELECT * FROM user WHERE email = '${req.body.email}'`
-  con.query(query, (err, result, fields) => {
-    var user = result[0]
-    res.send(user)
-  })
-  console.log(req.body.email + " " + req.body.password)
-})
-
-// POST method route
-router.post('/', (req, res) => {
-  var email = req.body.email
-  var password = req.body.password
-  var query = `INSERT INTO user (email, password) VALUES ('${email}', '${password}')`
-  con.query(query, function (req, res) {
-    console.log('Insert row')
-  })
-  res.send('Post api')
-})
+router.get('/', (req, res) => getHandler(req, res))
+router.post('/', (req, res) => postHandler(req, res))
   
 // PUT method route
 router.put('/', function(req, res) {

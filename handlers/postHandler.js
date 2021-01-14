6b6fa9327380f538fdde6ad0475fd70d75fdedd1
shannonlee98 <shanklee.lj@gmail.com>
2021-01-14 11:47:35 +0800

    cleanup

--- handlers/postHandler.js
+++ handlers/postHandler.js
@@ -2,29 +2,6 @@ const con = require('../config/database')
 var LocalStorage = require('node-localstorage').LocalStorage,
 localStorage = new LocalStorage('./scratch');
 
-// function postHandler(req, res) {
-//   var email = req.body.email
-//   var password = req.body.password
-//   var query = `INSERT INTO user (email, password) VALUES ('${email}', '${password}')`
-//   con.query(query, function (req, res) {
-//     console.log('Insert row')
-//   })
-//   console.log('Post api: ' + email + ' ' + password)
-//   res.redirect('/')
-// }
-
-// function getHandler(req, res) {
-//   var email = req.query.email
-//   var password = req.query.password
-//   var query = `SELECT * FROM user WHERE email = '${req.query.email}'`
-//   con.query(query, (err, result, fields) => {
-//     var user = result[0]
-//     console.log('Found user: ' + user.email)
-//   })
-//   console.log('Get api: ' + email + ' ' + password)
-//   res.redirect('/profile')
-// }
-
 function signupHandler(req, res) {
   var email = req.body.email
   var password = req.body.password
@@ -75,8 +52,6 @@ function loginHandler(req, res) {
 }
 
 module.exports = {
-  // postHandler,
-  // getHandler,
   signupHandler,
   loginHandler
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// const post = require('../handlers/postHandler').postHandler
-// const get = require('../handlers/postHandler').getHandler
 const signup = require('../handlers/postHandler').signupHandler
 const login = require('../handlers/postHandler').loginHandler
 
@@ -11,8 +9,6 @@ router.get('/', (req, res) => {
   res.render('home.html')
 })
 
-// router.post('/api', (req, res) => post(req, res))
-// router.get('/api', (req, res) => get(req, res))
   
 router.get('/signup', (req, res) => res.render('signup.html'))
 router.post('/signup', (req, res) => signup(req, res))
@@ -27,15 +23,5 @@ router.get('/profile', (req, res) => {
 router.get('/logout', (req, res) => {
   res.redirect('/')
 })
-
-// PUT method route
-router.put('/', function(req, res) {
-  res.send('Update api')
-});
-
-// DELETE method route
-router.delete('/', function(req, res) {
-  res.send('Delete api')
-});
 
 module.exports = router

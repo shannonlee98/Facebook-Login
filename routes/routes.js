a2c25c617775b5d49df50199ae5fc0bdfb85ffe3
shannonlee98 <shanklee.lj@gmail.com>
2021-01-14 11:43:42 +0800

    html signup and login

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-const post = require('../handlers/postHandler').postHandler
-const get = require('../handlers/postHandler').getHandler
+// const post = require('../handlers/postHandler').postHandler
+// const get = require('../handlers/postHandler').getHandler
+const signup = require('../handlers/postHandler').signupHandler
+const login = require('../handlers/postHandler').loginHandler
 
-router.post('/api', (req, res) => post(req, res))
-router.get('/api', (req, res) => get(req, res))
+router.get('/', (req, res) => {
+  let sess = req.session
+  res.render('home.html')
+})
+
+// router.post('/api', (req, res) => post(req, res))
+// router.get('/api', (req, res) => get(req, res))
   
+router.get('/signup', (req, res) => res.render('signup.html'))
+router.post('/signup', (req, res) => signup(req, res))
+
+router.get('/login', (req, res) => res.render('login.html'))
+router.post('/login', (req, res) => login(req, res))
+
+router.get('/profile', (req, res) => {
+  res.render('profile.html', { user: req.session.user })
+})
+
+router.get('/logout', (req, res) => {
+  res.redirect('/')
+})
+
 // PUT method route
 router.put('/', function(req, res) {
   res.send('Update api')
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 const path = require('path')
+const session = require('express-session');
 
 const router = require('./routes/routes')
 
@@ -13,13 +14,23 @@ const router = require('./routes/routes')
 app.listen(port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({secret: 'anystringoftext',
+                saveUninitialized: true,
+                resave: true
+            }));
 app.use('/', router);
+
+app.use(express.static(__dirname + '/'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 
 
 // Routes
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/views/home.html'));
+  res.render('home.html')
+  // res.sendFile(path.join(__dirname+'/views/home.html'));
 });
 
 

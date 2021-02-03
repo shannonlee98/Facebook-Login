7b9776a1cc7f5505c457b098c35da94e2304dc6f
shannonlee98 <shanklee.lj@gmail.com>
2021-02-03 17:20:37 +0800

    gitignore

--- config/fbconfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    app_id: '1068263940305894',
-    client_secret: '7ae2ad17d7aa2ee02172bc4e4bf58513',
-    redirect_uri: 'https://shannon.vivita.sg/oauth-redirect',
-    state_param: 'login',
-    protocol: 'https'
-}
